3fd04be8d13cd69923c6eeebd8f978471a379da7
Ambika Maheshwari <ambika.maheshwari17@gmail.com>
2015-11-25 16:31:20 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
     $db_connection = array();
     $db_connection['host'] = "localhost";
-    $db_connection['username'] = "shruthi"; 
-    $db_connection['password'] = "shruthi"; 
+    $db_connection['username'] = ""; 
+    $db_connection['password'] = ""; 
     $db_connection['database'] = "rendezvous"; 
 ?>
