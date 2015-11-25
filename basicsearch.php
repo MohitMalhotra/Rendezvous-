9ba36e8a368414350d93cd34b507f3a0be7da066
Ambika Maheshwari <ambika.maheshwari17@gmail.com>
2015-11-25 16:45:27 -0500

    Basic Search - Shruthi

--- basicsearch.php
+++ basicsearch.php
@@ -1,8 +1,11 @@
+<?php
+session_start(); 
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Basic Search </title>
+  <title>Basic Search</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -13,24 +16,35 @@
 <div class="tabs">
 <ul class="list-inline">
   <li class="active"><a href="#">Basic Search</a></li>
-  <!-- <li><a href="#" role="button" class="btn popovers" data-toggle="popover" title="" data-content="test content <a href='' title='test add link'>link on content</a>" data-original-title="test title">Meet halfway</a></li> -->
-  <li><a href="#" role="button" class="btn popovers" data-toggle="popover" title="" data-content="<a href='homepage.php' title='test add link'>homepage.php</a>" data-original-title="Login required">Meet halfway</a></li>
+  <?php
+  if(!isset($_SESSION['login'])) { 
+  ?>
+  <li><a href="#" role="button" class="btn popovers" data-toggle="popover" title="" data-content="<a href='homepage.php' title='test add link'>Login</a>" data-original-title="Login required">Meet halfway</a></li>
+  <?php
+  }
+  else {
+  ?>
+  <li><a href="#">Meet halfway</a></li>
+  <?php
+  } 
+  ?>
 </ul>
 </div>
 
 <div class="tab-content">
     <div id="tab1" class="active">
-    <form>
-	Venue:<input type="search" name="venue"><br><br>
-	City: <input type="search" name="city"><br><br>
-	<button type="Submit" name="btn-search" onsubmit="searchoutput.php">Search</button>
+    <form method="post" action="" name="basicSearchForm" id="basicSearchForm">
+	Venue:<input type="search" name="venue" id="venue"><br><br>
+	City: <input type="search" name="city" id="city"><br><br>
+	<input type="submit" name="basicSearch" id="basicSearch" value="Search"/>
 	</form>
     </div>
 	
-    <div id="tab2" class="tab">
-    <form><br><br>
-	Zip/Address#1:<input type="search" name="address1"><br><br>
-	Zip/Address#2: <input type="search" name="address2"><br><br>
+    <div id="tab2" class="tab2">
+    <form method="post" action="" name="meethalfwayForm" id="meethalfwayForm">
+    <br><br>
+	Zip/Address#1:<input type="search" name="address1" id="address1"><br><br>
+	Zip/Address#2: <input type="search" name="address2" id="address1"><br><br>
 	
 	Radius:<select>
 	<option value="1">1 mile</option>
@@ -50,7 +64,8 @@
 	<option value="5">5 stars</option>
 	</select><br><br>
 	
-	<button type="Submit" name="btn-search">Search</button>
+	<input type="submit" name="meethalfwaySearch" id="meethalfwaySearch" value="Search" />
+	</form>
 	</div>
 </div>
 <script type="text/javascript">
