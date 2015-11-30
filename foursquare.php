7a06f68b3901f211e514045678c38eecec3f7fa9
Ambika Maheshwari <ambika.maheshwari17@gmail.com>
2015-11-30 13:53:57 -0500

    Adding Foursquare API call and Backgroundcover

---
+++ foursquare.php
@@ -0,0 +1,212 @@
+<?php
+ $client_ID = "K5DRB5U12ZDL34XSLS3C4IZB3YMJRIKCRLDJ3WCV4KXIAW4N";
+$client_secret = "I14SEXDCR23VH4IQSSMJ5MPDXIDUM0U4JCMG4FTLJGUNRK0R";
+$oauth_token = "34BKKF5OYKVTDBGZEWADDVHZB1NJQHZ2AEIOSOD0LRQ3T3KL";
+$version = "20151125";
+error_reporting ( E_ERROR | E_PARSE ); 
+class Foursquare {
+	
+
+	// search by name and city
+	public function NameCity($name,$city) {
+		$city = "chicago";
+		$name = "chipo";
+		$resp_oauthtoken = file_get_contents ( 'https://api.foursquare.com/v2/venues/explore?near=' . $city . '&query=' . $name . '&oauth_token=' . $oauth_token . '&v=' . $version );
+		$obj = json_decode ( $resp_oauthtoken, true );
+		
+		if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+			for($i = 0; $i < 30; $i ++) {
+				echo "<br/>";
+				echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['name'];
+				echo "<br/>";
+				echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'];
+			}
+		} else
+			echo "Please check the credential again !!";
+	}
+	
+	// Search by Zipcode + radius + rating
+	public function ZipRadiusRating() {
+		$zipcode = '07307';
+		$radius = 1 * 1609.34;
+		$rating = "8";
+		
+		$resp_latlnggoogle = file_get_contents ( 'https://maps.googleapis.com/maps/api/geocode/json?address=' . $zipcode . '&key=AIzaSyCqt0V2s8VlZHYEjC2k1k_rWhcSDVFxwfg' );
+		$objlatlng = json_decode ( $resp_latlnggoogle, true );
+		if ($objlatlng ['status'] == "OK") {
+			$lat = $objlatlng ['results'] ['0'] ['geometry'] ['location'] ['lat'];
+			$lng = $objlatlng ['results'] ['0'] ['geometry'] ['location'] ['lng'];
+			
+			$resp_oauthtoken = file_get_contents ( 'https://api.foursquare.com/v2/venues/explore?ll=' . $lat . ',' . $lng . '&radius=' . $radius . '&oauth_token=' . $oauth_token . '&v=' . $version );
+			$obj = json_decode ( $resp_oauthtoken, true );
+			
+			if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+				for($i = 0; $i < 30; $i ++) {
+					if (floatval ( $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'] ) >= floatval ( $rating )) {
+						echo "<br/>";
+						echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['name'];
+						echo "<br/>";
+						echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'];
+					}
+				}
+			} else
+				echo "Please check the credential again !!";
+		} else
+			echo "Please check the zipcode again !!";
+	}
+	// Advance search -- search by name + city + radius + rating
+	public function NameCityRadiusRating($name,$city,$radius,$rating) {
+		
+		$radius_new = $radius * 1609.34;
+	
+		$resultnamecityrr = array();
+		$url = "https://api.foursquare.com/v2/venues/explore?query=$name&radius=$radius_new&near=$city&oauth_token=34BKKF5OYKVTDBGZEWADDVHZB1NJQHZ2AEIOSOD0LRQ3T3KL&v=20151125&sortByDistance=1";
+		$resp_oauthtoken = file_get_contents ($url);
+		$obj = json_decode ( $resp_oauthtoken, true );
+		if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+			for($i = 0; $i < 30; $i ++) {
+				if (floatval ( $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'] ) >= floatval ( $rating )) {
+					array_push($resultnamecityrr,$obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['id']);
+			
+				}
+			}
+			return $resultnamecityrr;
+		} else
+			//return $url;
+			return "Please check the credential again !!";
+	}
+	// search by city + radius + rating
+	public function CityRadiusRating() {
+		$city = "chicago";
+		$radius = 1 * 1609.34;
+		$rating = "8";
+		$resp_oauthtoken = file_get_contents ( 'https://api.foursquare.com/v2/venues/explore?near=' . $city . '&radius=' . $radius . '&oauth_token=' . $oauth_token . '&v=' . $version );
+		$obj = json_decode ( $resp_oauthtoken, true );
+		
+		if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+			for($i = 0; $i < 30; $i ++) {
+				if (floatval ( $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'] ) >= floatval ( $rating )) {
+					echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['name'];
+					echo "<br/>";
+					echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'];
+					echo "<br/>";
+				}
+			}
+		} else
+			echo "Please check the credential again !!";
+	}
+	public function ZipNameRadiusRating($name,$zipcode,$radius,$rating) {
+		// Search by Zipcode and Name + Radius + Rating
+
+		$radius_new = $radius * 1609.34;
+		
+		$resultnameziprr = array();
+		$url_google = "https://maps.googleapis.com/maps/api/geocode/json?address=$zipcode&query=$name&key=AIzaSyCqt0V2s8VlZHYEjC2k1k_rWhcSDVFxwfg";
+		$resp_latlnggoogle = file_get_contents ( $url_google );
+		$objlatlng = json_decode ( $resp_latlnggoogle, true );
+		$lat = $objlatlng ['results'] ['0'] ['geometry'] ['location'] ['lat'];
+		$lng = $objlatlng ['results'] ['0'] ['geometry'] ['location'] ['lng'];
+		$url_fsq = "https://api.foursquare.com/v2/venues/explore?ll=$lat,$lng&radius=$radius_new&oauth_token=34BKKF5OYKVTDBGZEWADDVHZB1NJQHZ2AEIOSOD0LRQ3T3KL&v=20151125&sortByDistance=1";
+		$resp_oauthtoken = file_get_contents ($url_fsq);
+		$obj = json_decode ( $resp_oauthtoken, true );
+		
+		if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+			for($i = 0; $i < 30; $i ++) {
+				if (floatval ( $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'] ) >= floatval ( $rating )) {
+					array_push($resultnameziprr,$obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['name']);
+					
+				}
+			}
+			if(sizeOf($resultnameziprr) > 1)
+			{
+				return $resultnameziprr;}
+			else
+			{
+				echo $obj ['response']['warning']['text'];}
+			
+		} else
+			echo "Please check the credential again !!";
+	}
+	
+	// Search by Latitude and Longitude + radius + rating
+	public function latLongRadiusRating() {
+		$lat = "";
+		$lng = "";
+		$radius = "";
+		$rating = "";
+		$resp_oauthtoken = file_get_contents ( 'https://api.foursquare.com/v2/venues/explore?ll=' . $lat . ',' . $lng . '&radius=' . $radius . '&oauth_token=' . $oauth_token . '&v=' . $version );
+		$obj = json_decode ( $resp_oauthtoken, true );
+		
+		if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+			for($i = 0; $i < 30; $i ++) {
+				if (floatval ( $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'] ) >= floatval ( $rating )) {
+					echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['name'];
+					echo "<br/>";
+					echo $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'];
+				}
+			}
+		} 
+
+		else
+			echo "Please check the credential again !!";
+	}
+	// Search by lat and name + radius + rating
+	public function LatLongNameRadiusRating($lat,$long,$name,$radius,$rating) {
+		$radius_new = $radius * 1609.34;
+		
+		$resultllnamerr = array();
+		$url = "https://api.foursquare.com/v2/venues/explore?ll=$lat,$long&query=$name&radius=$radius_new&oauth_token=34BKKF5OYKVTDBGZEWADDVHZB1NJQHZ2AEIOSOD0LRQ3T3KL&v=20151125&sortByDistance=1";
+		$resp_oauthtoken = file_get_contents ($url);
+		$obj = json_decode ( $resp_oauthtoken, true );
+		
+		if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+			for($i = 0; $i < 30; $i ++) {
+				if (floatval ( $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'] ) >= floatval ( $rating )) {
+					array_push($resultllnamerr,$obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['name']);
+					
+				}
+			}
+		if(sizeOf($resultllnamerr) > 1)
+		{	return $resultllnamerr;}
+		else 
+		{
+			echo $obj ['response']['warning']['text'];} //error display
+		
+		} else
+			//echo $url;
+			echo "Please check the credential again !!";
+	}
+	
+	
+	// City + Lat + Lng + Rad + Rating
+	public function CityLatLngRadiusRating($lat,$long,$city,$radius,$rating) {
+	
+		$radius_new = $radius * 1609.34;
+	
+		$resultllcityrr = array();
+		$url = "https://api.foursquare.com/v2/venues/explore?ll=$lat,$long&radius=$radius_new&near=$city&oauth_token=34BKKF5OYKVTDBGZEWADDVHZB1NJQHZ2AEIOSOD0LRQ3T3KL&v=20151125&sortByDistance=1";
+		$resp_oauthtoken = file_get_contents ($url);
+		$obj = json_decode ( $resp_oauthtoken, true );
+		if (floatval ( $obj ['meta'] ['code'] ) == 200) {
+			for($i = 0; $i < 30; $i ++) {
+				if (floatval ( $obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['rating'] ) >= floatval ( $rating )) {
+					array_push($resultllcityrr,$obj ['response'] ['groups'] ['0'] ['items'] [$i] ['venue'] ['name']);
+						
+				}
+			}
+			if(sizeOf($resultllcityrr) > 1)
+			{				
+				return $resultllcityrr;
+			}
+			else
+			{
+				echo $obj ['response']['warning']['text'];} //error display
+			
+		} else
+			//return $url;
+			return "Please check the credential again !!";
+	}
+	
+}
+// 1 mile = 1609.34 meters
+?>
